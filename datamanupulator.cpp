002eab2652aaf7b5759663c39fd732ed6d2fdf85
Rohith2004donka <158990825+Rohith2004donka@users.noreply.github.com>
2024-02-21 22:19:09 +0530

    datamanupulator.cpp

--- datamanupulator.cpp
+++ datamanupulator.cpp
@@ -1,10 +1,40 @@
+
 #include<iostream>
+#include<iomanip>
 using namespace std;
+
 int main()
 {
-	cout<<"USING '\\n' ...\n";
-	cout<<"Line 1 \nLine 2 \nLine 3 \n";
-	cout<<"USING end ..."<< endl;
-	cout<<"Line 1"<<endl<<"Line 2"<<endl<<"Line 3"<<endl;
+    //using endl
+    cout<<"My name is Sasi"<<endl;
+    //
+    //using ends
+    cout<<"Hi"<<ends;
+    cout<<endl;
+    //terminates by inserting null character
+    //using ws
+    string name;
+    getline(cin>>ws,name);
+    cout<<name<<endl;
+    //removes whitespaces if they are present
+    //setw
+    cout<<setw(6)<<"Hi\n";
+    //creates spaces provides in setw(n)
+    //setfill
+    cout<<setfill('^')<<setw(12)<<"Hi sasi\n";
+    //fills the space given by setw
+    //setprecision
+    double num=0.233423423424;
+    cout<<setprecision(4)<<num<<endl;
+    //setprecision decides the values after the decimal points
+    //flush
+    int a=10;
+    cout.flush();
+    cout<<"The value of a is "<<a;
+    return 0;
 }
+
+
+
+
 
