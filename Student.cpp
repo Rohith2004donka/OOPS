248b0b140bf8177c28f9ba70ff6248a8faa87a49
Rohith2004donka <158990825+Rohith2004donka@users.noreply.github.com>
2024-02-21 21:00:28 +0530

    Create Student.cpp

---
+++ Student.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+class Student
+{-
+    public:
+        string fullName;
+        int rollNum;
+        double semPerentage;
+        string collegeName;
+        int collegeCode;
+        Student()
+        {
+            cout<<"Student class initialized!!\n";
+        }
+        ~Student(){}
+};
+
+int main()
+{
+    Student rohith;
+    cout<<"Enter your name : ";
+    getline(cin, rohith.fullName);
+    cout<<"Enter your roll number : ";
+    cin>>rohith.rollNum;
+    cout<<"Enter your sem percentage : ";
+    cin>>rohith.semPerentage;
+    cout<<"Enter your college name : ";
+    cin>>rohith.collegeName;
+    cout<<"Enter your college code : ";
+    cin>>rohith.collegeCode;
+    cout<<"Name : "<<rohith.fullName<<endl;
+    cout<<"RollNum : "<<rohith.rollNum<<endl;
+    cout<<"SemPercentage : "<<rohith.semPerentage<<endl;
+    cout<<"CollegeName : "<<rohith.collegeName<<endl;
+    cout<<"CollegeCode : "<<rohith.collegeCode<<endl;
+    return 0;
+}
